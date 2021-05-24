8131eecce5e65bd041b8b82becdb88180f29bb95
open seven six ate <open768@yahoo.co.uk>
2021-05-24 18:29:26 +0100

    Update facebook.php

--- ckinc/facebook.php
+++ ckinc/facebook.php
@@ -3,6 +3,7 @@ require_once("$phpinc/ckinc/debug.php");
 require_once("$phpinc/ckinc/secret.php");
 require_once("$phpinc/ckinc/common.php");
 require_once("$phpinc/facebook/autoload.php");
+require_once("$phpinc/ckinc/objstoredb.php");
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;	
@@ -13,9 +14,22 @@ use Facebook\GraphUser;
 //###########################################################################
 class cFacebook_ServerSide{
 	const FB_USER_FOLDER = "[facebook]";
+	const FB_ALL_USERS= "[all users]";
 	const FB_SESS_USER = "fbuser";
 	const FB_SESS_USERID = "fbuserid";
+	static $oObjStore = null;
 	
+	static function pr_init_objstore(){
+		cDebug::enter();
+		if (self::$oObjStore == null){
+			$oStore = new cObjStoreDB();
+			$oStore->realm = "FB";
+			//$oStore->SHOW_SQL = true; //DEBUG
+			$oStore->set_table("FB");	//possibly auth is a reserved name
+			self::$oObjStore = $oStore;
+		}
+		cDebug::leave();
+	}
 	//*******************************************************************
 	public static function is_facebook(){
 		//force on query string
@@ -28,24 +42,31 @@ class cFacebook_ServerSide{
 	
 	//*******************************************************************
 	private static function pr_setSessionUser($poGraphObject){
-		$sFirstName = $poGraphObject->getProperty("first_name");
-		$sLastName = $poGraphObject->getProperty("last_name");
+		cDebug::enter();
+		$sUser = $poGraphObject->getProperty("name");
 		$sID = $poGraphObject->getProperty("id");
-		$sUser = "$sFirstName $sLastName";
 		$_SESSION[self::FB_SESS_USER] = $sUser;
 		$_SESSION[self::FB_SESS_USERID] = $sID;
+		cDebug::leave();
 		return $sUser;
 	}
 	
 	//*******************************************************************
 	public static function getSessionUser(){
+		cDebug::enter();
+		$sUser = null;
 		//get the user from the session
-		if (isset($_SESSION[self::FB_SESS_USER]))			
-			return $_SESSION[self::FB_SESS_USER];
-		else{
+		if (isset($_SESSION[self::FB_SESS_USER]))	
+			if (! cDebug::$IGNORE_CACHE){
+				cDebug::extra_debug("using session user");
+				$sUser = $_SESSION[self::FB_SESS_USER];
+			}else
+				cDebug::extra_debug("ignoring session user");
+		else
 			cDebug::write("no facebook session user");
-			return null;
-		}
+		
+		cDebug::leave();
+		return $sUser;
 	}
 	
 	//*******************************************************************
@@ -61,53 +82,65 @@ class cFacebook_ServerSide{
 	
 	//*******************************************************************
 	public static function getStoredUser($psUserID){
-	
-		$sUser = self::getSessionUser();
-				
-		//is this user details stored?
-		if (!$sUser){
-			cDebug::write("username not in session, checking if known");
+		cDebug::enter();
 		
+		if (cDebug::$IGNORE_CACHE)
+			cDebug::extra_debug("nocache - not looking here");
+		else{
+			$sUser = self::getSessionUser();
+					
+			//is this user details stored?
+			if (!$sUser){
+				cDebug::write("username not in session, checking if known");
 			
-			$oGraphObject = cObjStore::get_file(self::FB_USER_FOLDER, $psUserID);
-			if ($oGraphObject){
-				cDebug::write("found stored user");
-				//$aNames = $oGraphObject->getPropertyNames();
-				$sUser = self::pr_setSessionUser($oGraphObject);
+				$oGraphObject = self::$oObjStore->get_oldstyle(self::FB_USER_FOLDER, $psUserID);
+				if ($oGraphObject){
+					cDebug::write("found stored user");
+					//$aNames = $oGraphObject->getPropertyNames();
+					$sUser = self::pr_setSessionUser($oGraphObject);
+				}
 			}
 		}
 		
+		cDebug::leave();
 		return $sUser;
 	}
 	
 	//*******************************************************************
 	private static function pr_storeUserDetails($psUserID, $poData){
+		cDebug::enter();
 		//store the details
-		cObjStore::put_file(self::FB_USER_FOLDER,$psUserID, $poData);
+		self::$oObjStore->put_oldstyle(self::FB_USER_FOLDER,$psUserID, $poData);
 		
-		//add to list of FB users (using hash to obfuscate for security)
-		$aFBApp = self::getAppID();
-		$sAllUsersFile = "AllFBusers".$aFBApp["S"];
-		$aFBUsers = cHash::get($sAllUsersFile);
+		//add to list of FB users
+		$aFBUsers = self::$oObjStore->get_oldstyle(self::FB_USER_FOLDER, self::FB_ALL_USERS);
 		if (!$aFBUsers) $aFBUsers = [];
 		$aFBUsers[$psUserID] = 1;
-		cHash::put($sAllUsersFile, $aFBUsers, true);
+		self::$oObjStore->put_oldstyle(self::FB_USER_FOLDER, self::FB_ALL_USERS, $aFBUsers);
+
+		cDebug::leave();
 	}
 	
 	//*******************************************************************
 	public static function getAppID(){
+		$sID = null;
+		cDebug::enter();
 		if (cHeader::is_localhost()){
 			cDebug::write("using development credentials for localhost");
-			return ["I"=>cSecret::$FB_DEV_APP, "S"=>cSecret::$FB_DEV_SECRET];
+			$sID = ["I"=>cSecret::$FB_DEV_APP, "S"=>cSecret::$FB_DEV_SECRET];
 		}else
-			return  ["I"=>cSecret::$FB_APP, "S"=>cSecret::$FB_SECRET];
+			$sID =   ["I"=>cSecret::$FB_APP, "S"=>cSecret::$FB_SECRET];
+		cDebug::leave();
+		return $sID;
 	}
 	
 	//*******************************************************************
 	public static function getUserIDDetails($psUserID, $psToken){
+		cDebug::enter();
 		$aFBApp = self::getAppID();
-		FacebookSession::setDefaultApplication($aFBApp["I"], $aFBApp["S"]);
-		cDebug::write("FBAPP: ".$aFBApp["I"]);
+
+		cDebug::extra_debug("-- validating session");
+		FacebookSession::setDefaultApplication($aFBApp["I"], $aFBApp["S"]);		
 				
 		$oSession = new FacebookSession($psToken);
 		try {
@@ -118,16 +151,21 @@ class cFacebook_ServerSide{
 		cDebug::write("FB session OK");
 		
 		//get the details of the user from facebook 
+		cDebug::extra_debug("-- requesting from Graph");
 		$oFBRequest = new FacebookRequest( $oSession, 'GET', '/me');
 		$oFBResponse = $oFBRequest->execute();
 		$oGraphObject = $oFBResponse->getGraphObject();
 		//cDebug::vardump($oGraphObject);
 		
 		//remember this user
+		cDebug::extra_debug("-- remembering user");
 		$sUser = self::pr_setSessionUser($oGraphObject);
 		self::pr_storeUserDetails( $psUserID, $oGraphObject);
 		
+		cDebug::leave();
 		return $sUser;
 	}
 }
+cFacebook_ServerSide::pr_init_objstore();
+
 ?>
