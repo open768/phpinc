f90460c5200e98b58b0f91b49dd5d3cd2acd31ee
sunil <cluck@chickenkatsu.co.uk>
2024-08-11 13:54:37 +0100

    small changes

--- ckinc/common.php
+++ ckinc/common.php
@@ -15,8 +15,7 @@ For licenses that allow for commercial use please contact cluck@chickenkatsu.co.
 require_once  "$phpInc/ckinc/debug.php";
 require_once  "$phpInc/ckinc/header.php";
 
-class cCommon
-{
+class cCommon {
     public static $SHOW_PROGRESS = TRUE;
     const SECONDS_IN_MONTH = 31 * 24 * 60 * 60;
     const ENGLISH_DATE_FORMAT = "d/m/Y H:i";
@@ -34,15 +33,13 @@ class cCommon
     const PROGRESS_CHAR = "&#8667;";
 
     //**************************************************************************
-    public static function filename()
-    {
+    public static function filename() {
         global $_SERVER;
         return basename($_SERVER["PHP_SELF"]);
     }
 
     //**************************************************************************
-    public static function write_json($poThing)
-    {
+    public static function write_json($poThing) {
         if (cDebug::is_debugging()) {
             cDebug::write("json output:");
             cDebug::vardump($poThing, true);
@@ -51,8 +48,7 @@ class cCommon
     }
 
     //**************************************************************************
-    public static function serialise($poThing)
-    {
+    public static function serialise($poThing) {
         if (!is_object($poThing)) cDebug::error("not an object");
         return get_object_vars($poThing);
     }
@@ -60,8 +56,7 @@ class cCommon
 
     //**************************************************************************
     //code for following function based on http://php.net/manual/en/function.rmdir.php
-    public static function delTree($psDir)
-    {
+    public static function delTree($psDir) {
         $aFiles = array_diff(scandir($psDir), array('.', '..'));
         foreach ($aFiles as $sfile) {
             try {
@@ -77,8 +72,7 @@ class cCommon
     }
 
     //**************************************************************************
-    public static function flushprint($psWhat = self::PROGRESS_CHAR)
-    {
+    public static function flushprint($psWhat = self::PROGRESS_CHAR) {
 
         if (self::$SHOW_PROGRESS) {
             print $psWhat;
@@ -87,15 +81,13 @@ class cCommon
     }
 
     //**************************************************************************
-    public static function do_echo($psWhat)
-    {
+    public static function do_echo($psWhat) {
         print "$psWhat\n";
         if (cDebug::is_debugging()) print "<br>";
     }
 
     //**************************************************************************
-    public static function get_session($psKey)
-    {
+    public static function get_session($psKey) {
         global $_SESSION;
 
         if (isset($_SESSION[$psKey]))
@@ -105,35 +97,30 @@ class cCommon
     }
 
     //**************************************************************************
-    public static function is_numeric_name($psName)
-    {
+    public static function is_numeric_name($psName) {
         return (preg_match('/\/\d+/', $psName, $matches) || preg_match('/\-\d+/', $psName, $matches));
     }
 
     //**************************************************************************
-    public static function is_string_set($psValue)
-    {
+    public static function is_string_set($psValue) {
         $bResult = (($psValue != null) && ($psValue !== ""));
         //if (!$bResult) cDebug::write("string: $psValue result: not set");
         return $bResult;
     }
 
     //**************************************************************************
-    public static function reformat_date($psDate, $psOldFormat, $psNewFormat)
-    {
+    public static function reformat_date($psDate, $psOldFormat, $psNewFormat) {
         $oDate = DateTime::createFromFormat($psOldFormat, $psDate);
         return $oDate->format($psNewFormat);
     }
 
     //**************************************************************************
-    public static function strip_non_printing($psIn)
-    {
+    public static function strip_non_printing($psIn) {
         return preg_replace('/[\x00-\x1F\x80-\x9F]/u', '', $psIn);
     }
 
     //**************************************************************************
-    public static function get_first_alphabet_char($psInput)
-    {
+    public static function get_first_alphabet_char($psInput) {
         if (preg_match('/[a-z]/i', $psInput, $match))
             return  $match[0];
         else
@@ -141,14 +128,12 @@ class cCommon
     }
 
     //**************************************************************************
-    public static function my_IP_address()
-    {
+    public static function my_IP_address() {
         return GetHostByName(null);
     }
 
     //**************************************************************************
-    public static function save_to_session($psKey, $psValue)
-    {
+    public static function save_to_session($psKey, $psValue) {
         global $_SESSION;
         cDebug::extra_debug("saving: $psKey =&gt; $psValue");
         $_SESSION[$psKey] = $psValue;
@@ -161,17 +146,23 @@ class cCommon
 //###################################################################################
 //#
 //###################################################################################
-class cPageOutput
-{
+class cPageOutput {
+    public static function prevent_buffering() {
+        ob_end_clean();
+        ini_set("max_execution_time", 60);
+        ini_set("max_input_time", 60);
+        set_time_limit(600);
+        ini_set("output_buffering", "Off");
+        ini_set("implicit_flush", 1);
+    }
+
     //**************************************************************************
-    public static function warning($psText)
-    {
+    public static function warning($psText) {
         cDebug::write("<b><font size='+2'color='brick'>Warning:</font></b> $psText");
     }
 
     //**************************************************************************
-    public static function put_in_wbrs($psInput, $piInterval = 20)
-    {
+    public static function put_in_wbrs($psInput, $piInterval = 20) {
         if (substr_count($psInput, " ") > 0)
             return $psInput;
         else {
@@ -181,15 +172,13 @@ class cPageOutput
         }
     }
     //**************************************************************************
-    public static function fixed_width_div($piWidth, $psContent)
-    {
+    public static function fixed_width_div($piWidth, $psContent) {
         $sBroken = self::put_in_wbrs($psContent);
         echo "<div style='width:{$piWidth}px;max-width:{$piWidth}px;'>{$sBroken}</div>";
     }
 
     //**************************************************************************
-    public static function write_JS_class_constant_IDs($psClassName)
-    {
+    public static function write_JS_class_constant_IDs($psClassName) {
         $oReflection = new ReflectionClass($psClassName);
         $aConsts = $oReflection->getConstants();
         if (count($aConsts) == 0) return;
@@ -207,8 +196,7 @@ class cPageOutput
     }
 
     //**************************************************************************
-    public static function errorbox($psMessage)
-    {
+    public static function errorbox($psMessage) {
 ?>
         <p>
         <div class='w3-panel w3-red w3-leftbar'>
@@ -221,14 +209,12 @@ class cPageOutput
     }
 
     //**************************************************************************
-    public static function div_with_cols($piCols, $psExtra = "")
-    {
+    public static function div_with_cols($piCols, $psExtra = "") {
         echo "<div style='column-count:$piCols;overflow-wrap:break-word' $psExtra>";
     }
 
     //**************************************************************************
-    public static function messagebox($psMessage)
-    {
+    public static function messagebox($psMessage) {
     ?>
         <p>
         <div class='w3-panel w3-blue w3-round-large w3-padding-16 w3-leftbar'>
