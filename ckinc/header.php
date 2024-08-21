b1629565d81c985537dcb3ad6d494276715a5b83
sunil <cluck@chickenkatsu.co.uk>
2024-08-21 22:26:52 +0100

    fixed https://github.com/open768/curiosity_browser/issues/9

--- ckinc/header.php
+++ ckinc/header.php
@@ -36,89 +36,91 @@ class cHeader {
             exit;
         }
         if (headers_sent()) {
-?><script>
-                document.location = "<?= $psUrl ?>";
-            </script><?php
-                    } else
-                        header("Location: $psUrl");
-                }
+            echo "<script>document.location='", $psUrl, "'</script>";
+        } else
+            header("Location: $psUrl");
+    }
 
-                //*******************************************************************
-                public static function is_set($psKey) {
-                    return (isset($_GET[$psKey]) || isset($_POST[$psKey]));
-                }
+    //*******************************************************************
+    public static function is_set($psKey) {
+        return (isset($_GET[$psKey]) || isset($_POST[$psKey]));
+    }
 
-                //*******************************************************************
-                public static function get($psKey) {
-                    $sValue = null;
-                    if (isset($_GET[$psKey]))
-                        $sValue = $_GET[$psKey];
-                    else if (isset($_POST[$psKey]))
-                        $sValue = $_POST[$psKey];
-                    else {
-                        cDebug::extra_debug("key:$psKey not found in GET or POST");
-                        return null;
-                    }
+    //*******************************************************************
+    public static function count_params() {
+        return count($_GET) + count($_POST);
+    }
 
-                    if ($sValue == null)
-                        return null;
+    //*******************************************************************
+    public static function get($psKey) {
+        $sValue = null;
+        if (isset($_GET[$psKey]))
+            $sValue = $_GET[$psKey];
+        else if (isset($_POST[$psKey]))
+            $sValue = $_POST[$psKey];
+        else {
+            cDebug::extra_debug("key:$psKey not found in GET or POST");
+            return null;
+        }
 
-                    while (strstr($sValue, "%")) {
-                        $sDecoded = urldecode($sValue);    //reverse proxy double encodes strings
-                        if ($sDecoded == $sValue)
-                            break;
-                        $sValue = $sDecoded;
-                    }
-                    return $sValue;
-                }
+        if ($sValue == null)
+            return null;
 
-                //*******************************************************************
-                public static function get_server() {
-                    $server_name = $_SERVER['SERVER_NAME'];
+        while (strstr($sValue, "%")) {
+            $sDecoded = urldecode($sValue);    //reverse proxy double encodes strings
+            if ($sDecoded == $sValue)
+                break;
+            $sValue = $sDecoded;
+        }
+        return $sValue;
+    }
 
-                    if (!in_array($_SERVER['SERVER_PORT'], [80, 443])) {
-                        $port = ":$_SERVER[SERVER_PORT]";
-                    } else {
-                        $port = '';
-                    }
+    //*******************************************************************
+    public static function get_server() {
+        $server_name = $_SERVER['SERVER_NAME'];
 
-                    if (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')) {
-                        $scheme = 'https';
-                    } else {
-                        $scheme = 'http';
-                    }
-                    return $scheme . '://' . $server_name . $port;
-                }
+        if (!in_array($_SERVER['SERVER_PORT'], [80, 443])) {
+            $port = ":$_SERVER[SERVER_PORT]";
+        } else {
+            $port = '';
+        }
 
-                //*******************************************************************
-                public static function get_page_url() {
-                    $sServer = self::get_server();
+        if (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')) {
+            $scheme = 'https';
+        } else {
+            $scheme = 'http';
+        }
+        return $scheme . '://' . $server_name . $port;
+    }
 
-                    return $sServer . $_SERVER["SCRIPT_NAME"];
-                }
-                //*******************************************************************
-                public static function get_page_dir() {
-                    return cHeader::get_server() . dirname($_SERVER["SCRIPT_NAME"]);
-                }
+    //*******************************************************************
+    public static function get_page_url() {
+        $sServer = self::get_server();
+
+        return $sServer . $_SERVER["SCRIPT_NAME"];
+    }
+    //*******************************************************************
+    public static function get_page_dir() {
+        return cHeader::get_server() . dirname($_SERVER["SCRIPT_NAME"]);
+    }
 
 
-                //*******************************************************************
-                public static function set_download_filename($psFilename) {
-                    if (cDebug::is_debugging()) {
-                        cDebug::write("download file would have been set to $psFilename");
-                        return;
-                    }
+    //*******************************************************************
+    public static function set_download_filename($psFilename) {
+        if (cDebug::is_debugging()) {
+            cDebug::write("download file would have been set to $psFilename");
+            return;
+        }
 
-                    if (headers_sent()) cDebug::error("Cant set filename, headers sent");
-                    header("Content-type: text/csv");
-                    header("Content-Disposition: attachment;filename=$psFilename");
-                    header("Pragma: no-cache");
-                    header("Expires: 0");
-                }
+        if (headers_sent()) cDebug::error("Cant set filename, headers sent");
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment;filename=$psFilename");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+    }
 
-                //*******************************************************************
-                public static function get_referer() {
-                    return $_SERVER["HTTP_REFERER"];
-                }
-            }
-                        ?>
+    //*******************************************************************
+    public static function get_referer() {
+        return $_SERVER["HTTP_REFERER"];
+    }
+}
